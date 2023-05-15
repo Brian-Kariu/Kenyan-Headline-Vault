95702c3dd1f7bd81276a44bc588bee6d139cd132
Brian Kariu <waiganjo.brian@gmail.com>
2023-05-15 06:06:18 +0300

    feat: Added Authentication flow
    * Added firebase
    * First steps towards authentication

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const authentication = require("./auth/index")
+const { initializeApp } = require('firebase/app');
 const scraperRoutes = require("./app/scraper");
-const app = express();
 const port = 4000;
 
+const firebaseConfig = {
+  apiKey:process.env.APIKEY,
+  authDomain:process.env.AUTHDOMAIN,
+  projectId:process.env.PROJECTID,
+  storageBucket:process.env.STORAGEBUCKET,
+  messagingSenderId:process.env.MESSAGINGSENDERID,
+  appId:process.env.APPID,
+  measurementId:process.env.MEASUREMENTID
+}
+
+const app = initializeApp(firebaseConfig);
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use("/scrape", scraperRoutes.scraperRoute);
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
